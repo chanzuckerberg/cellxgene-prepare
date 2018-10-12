434efe1a1b05d75bb74f088b26be17765a98778a
freeman-lab <the.freeman.lab@gmail.com>
2018-10-12 01:58:14 -0400

    add option

--- prepare/cli.py
+++ prepare/cli.py
@@ -7,11 +7,20 @@ settings = dict(help_option_names=['-h', '--help'])
 
 @click.command()
 @click.argument('dataset', nargs=1, metavar='<dataset: name of dataset>', required=True)
-def cli(dataset):
+@click.option('--overwrite/--no-overwrite', default=True, help='whether or not to overwrite file')
+def cli(dataset, overwrite):
     """
     Hi! This is a tool for preprocessing data for use with cellxgene.
     """
     print(read_h5ad(dataset))
+
+    # here's where the business logic will go
+
+    # load the dataset
+
+    # perform any precomputation
+
+    # save results or overwrite
 
 if __name__ == '__main__':
   cli()
