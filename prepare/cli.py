6f646a783f339dd7933627a80568609ffbf55755
freeman-lab <the.freeman.lab@gmail.com>
2018-10-17 16:11:42 -0700

    first working version of cli

--- prepare/cli.py
+++ prepare/cli.py
@@ -1,26 +1,89 @@
 import click
 
 from os.path import isfile
-from scanpy.api import read_h5ad
 
 settings = dict(help_option_names=['-h', '--help'])
 
 @click.command()
 @click.argument('dataset', nargs=1, metavar='<dataset: name of dataset>', required=True)
-@click.option('--overwrite/--no-overwrite', default=True, help='whether or not to overwrite file')
-def cli(dataset, overwrite):
+@click.option('--engine', default='scanpy', type=click.Choice(['scanpy']), help='computational engine', show_default=True)
+@click.option('--format', default='h5ad', type=click.Choice(['h5ad', '10x_mtx', 'loom']), help='data format', show_default=True)
+@click.option('--layout', default='umap', type=click.Choice(['umap', 'tsne', 'umap+tsne']), help='layout algorithm', show_default=True)
+@click.option('--recipe', default='none', type=click.Choice(['none', 'seurat', 'zheng17']), help='preprocessing receipe to run', show_default=True)
+@click.option('--output', default='', help='whether or not to save a new file')
+@click.option('--sparse/--no-sparse', default=False, help='whether to require sparsity')
+@click.option('--plotting/--no-plotting', default=False, help='whether or not to generate plots')
+def cli(dataset, engine, format, layout, recipe, output, sparse, plotting):
     """
     Hi! This is a tool for preprocessing data for use with cellxgene.
     """
-    print(read_h5ad(dataset))
+    import matplotlib
+    matplotlib.use('Agg')
+    import scanpy.api as sc
+    import pandas as pd
+    import numpy as np
 
-    # here's where the business logic will go
+    # scanpy settings 
+    sc.settings.verbosity = 2
+    sc.settings.autosave = True
 
-    # load the dataset
+    # data loading
+    adata = None
 
-    # perform any precomputation
+    if format == 'h5ad':
+        adata = sc.read_h5ad(dataset)
+    if format == '10x_mtx':
+        adata = sc.read_10x_mtx(dataset)
 
-    # save results or overwrite
+    adata.var_names_make_unique()
+
+    # run a recipe if requested
+    if recipe == 'seurat':
+        sc.pp.recipe_seurat(adata)
+    elif recipe == 'zheng17':
+        sc.pp.recipe_zheng17(adata)
+    else:
+        sc.pp.filter_cells(adata, min_genes=5)
+        sc.pp.filter_genes(adata, min_cells=25)
+        if sparse:
+            sc.pp.scale(adata, zero_center=False)
+        else:
+            sc.pp.scale(adata)
+
+    # dimensionality reduction
+    if sparse:
+        sc.pp.pca(adata, svd_solver='arpack', zero_center=False)
+    else:
+        sc.pp.pca(adata, svd_solver='arpack')
+
+    # neighbors and clustering
+    sc.pp.neighbors(adata)
+    sc.tl.louvain(adata)
+
+    # layout and plotting
+    if len(np.unique(adata.obs['louvain'].values)) < 10:
+        palette = 'tab10'
+    else:
+        palette = 'tab20'
+
+    if layout == 'umap' or layout == 'umap+tsne':
+        sc.tl.umap(adata)
+        if plotting:
+            sc.pl.umap(adata, color='louvain', palette=palette, save='_louvain')
+
+    if layout == 'tsne' or layout == 'umap+tsne':
+        sc.tl.tsne(adata)
+        if plotting:
+            sc.pl.tsne(adata, color='louvain', palette=palette, save='_louvain')    
+
+    # show the structure
+    print('data structure...')
+    print(adata)
+
+    # saving file
+    if not output == '':
+        print('saving output...')
+        adata.write(output)
 
 if __name__ == '__main__':
   cli()
